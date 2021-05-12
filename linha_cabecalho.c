6fd90a6a3cd3dae94d717d159f628569f9bafc8d
Tutu <turquetivitor@gmai.com>
2021-05-12 16:41:20 -0300

    esqueleto e scripts de headers

---
+++ linha_cabecalho.c
@@ -0,0 +1,32 @@
+struct _linha_cabecalho
+{
+    /* data */
+};
+
+
+/*
+    descricao:
+    essa func cria um cabecalho do arquivo linha
+    
+    argumentos:
+    [in] arg1
+    [out] arg2
+    
+    retorno:
+    se tudo der certo retorna 1 se algo der errado retorna -1
+*/
+void create_linha_cabecalho(){
+
+}
+
+void read_linha_cabecalho(){
+
+}
+
+void update_linha_cabecalho(){
+
+}
+
+void delete_linha_cabecalho(){
+
+}
---
+++ linha_cabecalho.h
@@ -0,0 +1,11 @@
+#ifndef LINHA_CABECALHO_H
+#define LINHA_CABECALHO_H
+
+typedef struct _linha_cabecalho LINHA_CABECALHO;
+
+void create_linha_cabecalho();
+void read_linha_cabecalho();
+void update_linha_cabecalho();
+void delete_linha_cabecalho();
+
+#endif
